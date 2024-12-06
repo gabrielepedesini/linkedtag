d6f67b8acc85b55896cf7306c2b3d3d157c0046a
gabrielepedesini <gabrismile23@gmail.com>
2024-12-06 23:55:02 +0100

    frontend major updates

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+//retrieve hashtags function
 document.getElementById('post-form').addEventListener('submit', () => {
     event.preventDefault();
 
@@ -6,8 +7,6 @@ document.getElementById('post-form').addEventListener('submit', () => {
 
     cleanTitle = title.normalize('NFKD').replace(/(\r\n|\n|\r)/gm, ' ').replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '');
     cleanContent = content.normalize('NFKD').replace(/(\r\n|\n|\r)/gm, ' ').replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '');
-
-    console.log(cleanContent)
 
     fetch('/generate-hashtags', {
         method: 'POST',
@@ -18,14 +17,19 @@ document.getElementById('post-form').addEventListener('submit', () => {
     .then(data => {
         const table = document.getElementById('results-table');
         const tbody = table.querySelector('tbody');
+        const copyBtn = document.getElementById('copy-btn');
+        const copyOut = document.getElementById('copy-output');
         tbody.innerHTML = '';
+        copyOut.innerText = '';
 
         if (data.length > 0) {
             table.style.display = 'table';
+            copyBtn.style.display = 'block';
 
             data.forEach(row => {
                 const tr = document.createElement('tr');
                 tr.innerHTML = `
+                    <td><input type="checkbox" value="${'#' + row.hashtag}" checked></td>
                     <td>${'#' + row.hashtag}</td>
                     <td>${row.followers}</td>
                     <td>${(row.score * 100).toFixed(1) + '%'}</td>
@@ -38,3 +42,28 @@ document.getElementById('post-form').addEventListener('submit', () => {
         }
     });
 });
+
+//copy selected hashtags to clipboard
+const copyBtn = document.getElementById('copy-btn');
+const copyOut = document.getElementById('copy-output');
+
+copyBtn.addEventListener('click', () => {
+    const hashtags = document.querySelectorAll('input[type="checkbox"]:checked');
+
+    let buffer = '';
+
+    hashtags.forEach(el => {
+        buffer += el.value + " ";
+    })
+
+    copyOut.style.display = 'block';
+
+    //copy to clipboard
+    navigator.clipboard.writeText(buffer)
+        .then(() => {
+            copyOut.innerText = 'Copied!'
+        })
+        .catch(err => {
+            copyOut.innerText = 'Error!'
+        });
+})
